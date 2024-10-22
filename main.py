f4a92c69effb675c0783c7f5fbefae2d5c4cc2dd
dvarvarg <dvarvarg@gmail.com>
2024-10-22 22:17:59 +0300

    добавлены кнопки калькулятора

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ def calc():
         result=c.multiply(first,second)
     elif oper=='/':
         result=c.divide(first,second)
+    entry.delete(0,END)
+    entry.insert(str(result))
 
 
 # функция ввода числа
@@ -45,6 +47,26 @@ window.title('Калькулятор')
 window.geometry()
 
 entry=ttk.Entry()
-entry.pack()
+entry.grid(row=0,column=0,columnspan=4,sticky='ew')
+
+ttk.Button(text='1', command=lambda: enter_number('1')).grid(row=1,column=0)
+ttk.Button(text='2', command=lambda: enter_number('2')).grid(row=1,column=1)
+ttk.Button(text='3', command=lambda: enter_number('3')).grid(row=1,column=2)
+ttk.Button(text='4', command=lambda: enter_number('4')).grid(row=2,column=0)
+ttk.Button(text='5', command=lambda: enter_number('5')).grid(row=2,column=1)
+ttk.Button(text='6', command=lambda: enter_number('6')).grid(row=2,column=2)
+ttk.Button(text='7', command=lambda: enter_number('7')).grid(row=3,column=0)
+ttk.Button(text='8', command=lambda: enter_number('8')).grid(row=3,column=1)
+ttk.Button(text='9', command=lambda: enter_number('9')).grid(row=3,column=2)
+ttk.Button(text='0', command=lambda: enter_number('0')).grid(row=4,column=0)
+
+ttk.Button(text='C', command=clear_entry).grid(row=4,column=1)
+ttk.Button(text='=', command=calc).grid(row=4,column=2)
+
+ttk.Button(text='+', command=lambda: set_operation('+')).grid(row=1,column=3)
+ttk.Button(text='-', command=lambda: set_operation('-')).grid(row=2,column=3)
+ttk.Button(text='*', command=lambda: set_operation('*')).grid(row=3,column=3)
+ttk.Button(text='/', command=lambda: set_operation('/')).grid(row=4,column=3)
+
 
 window.mainloop()
